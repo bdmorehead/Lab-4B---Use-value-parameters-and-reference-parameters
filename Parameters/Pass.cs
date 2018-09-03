e44ccaf87452d1b7e5ca294ec4d2c77d8c8a1a57
Bobby Morehead <bobby.morehead@outlook.com>
2018-09-02 22:31:05 -0400

    Complete

--- Parameters/Pass.cs
+++ Parameters/Pass.cs
@@ -4,6 +4,13 @@ namespace Parameters
 {
     class Pass
     {
-        // TODO:
+        public static void Value(ref int param)
+        {
+            param = 42;
+        }
+        public static void Reference(WrappedInt param)
+        {
+            param.Number = 42;
+        }
     }
 }
